26c765123f1a831c82ba3b00d8a23e6293692e21
Aryan Athane <aryanathane@gmail.com>
2026-02-12 19:58:17 +0530

    day 1 commit 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,23 @@
 import express from "express";
-import 'dotenv/config'
-
+import 'dotenv/config';
+import path from "path";
 const app=express();
 const port=process.env.PORT || 3000;
+
+const __dirname=path.resolve();
 
 app.get("/",(req,res)=>{
     res.send("we are live");
 })
+
+//make ready for deployment
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`); 
